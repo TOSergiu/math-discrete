80a55140fad13b7467ae31b382700694200e4c66
TOSergiu <sergiu-adrian.todoran@student.uclouvain.be>
2025-12-04 15:56:59 +0100

    main

---
+++ fonction.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def pageRankLinear(a: np.matrix, alpha: float, v: np.array):
+    """
+        – Input : Une matrice d’adjacence 1 A d’un graphe dirigé, 
+        pondéré et régulier G, un vecteur de personnalisation v, 
+        ainsi qu’un paramètre de téléportation α compris entre 0
+        et 1 (0.9 par défaut et pour les résultats à présenter).
+        Toutes ces valeurs sont non-négatives.
+
+        – Output : Un vecteur x contenant les scores d’importance des noeuds ordonnés dans
+        le même ordre que les lignes de la matrice d’adjacence (représentant les noeuds).
+    """
+
+def pageRankPower():
+
+def randowWalk():
